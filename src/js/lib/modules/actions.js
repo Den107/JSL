7345d5938a63c0b63697f3fa520911b25938343e
Den107 <deniskrv@gmail.com>
2021-03-25 21:23:21 +0300

    add closest, find, siblings, index, eq methods

--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -1,32 +1,114 @@
 import JSL from '../core';
 
-JSL.prototype.on = function (eventName, callback) {
+JSL.prototype.html = function (content) {
   for (let i = 0; i < this.length; i++) {
-    if (!eventName || !callback) {
-      return this;
-    }
-    this[i].addEventListener(eventName, callback);
-  }
-  return this;
-};
-
-JSL.prototype.off = function (eventName, callback) {
-  for (let i = 0; i < this.length; i++) {
-    if (!eventName || !callback) {
-      return this;
-    }
-    this[i].removeEventListener(eventName, callback);
-  }
-  return this;
-};
-
-JSL.prototype.click = function (handler) {
-  for (let i = 0; i < this.length; i++) {
-    if (handler) {
-      this[i].addEventListener('click', handler);
+    if (content) {
+      this[i].innerHTML = content;
     } else {
-      this[i].click();
+      return this[i].innerHTML;
     }
   }
+  return this;
+};
+
+JSL.prototype.eq = function (i) {
+  const swap = this[i];
+  const objLength = Object.keys(this).length;
+
+  for (let i = 0; i < objLength; i++) {
+    delete this[i];
+  }
+
+  this[0] = swap;
+  this.length = 1;
+
+  return this;
+};
+
+JSL.prototype.index = function () {
+  const parent = this[0].parentNode;
+  const children = [...parent.children];
+
+  const findMyIndex = (item) => {
+    return item == this[0];
+  };
+
+  return children.findIndex(findMyIndex);
+};
+
+JSL.prototype.find = function (selector) {
+  let numberOfItems = 0;
+  let counter = 0;
+
+  const copyObj = Object.assign({}, this);
+
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].querySelectorAll(selector);
+    if (arr.length == 0) {
+      continue;
+    }
+
+    for (let j = 0; j < arr.length; j++) {
+      this[counter] = arr[j];
+      counter++;
+    }
+
+    numberOfItems += arr.length;
+  }
+  this.length = numberOfItems;
+
+  const objLength = Object.keys(this).length;
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+
+  return this;
+};
+
+JSL.prototype.closest = function (selector) {
+  let counter = 0;
+
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i].closest(selector);
+    if (this[i] === null) {
+      this[i] = false;
+    }
+    counter++;
+  }
+
+  const objLength = Object.keys(this).length;
+  for (; counter < objLength; counter++) {
+    delete this[counter];
+  }
+
+  return this;
+};
+
+JSL.prototype.siblings = function () {
+  let numberOfItems = 0;
+  let counter = 0;
+
+  const copyObj = Object.assign({}, this);
+
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].parentNode.children;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (copyObj[i] === arr[j]) {
+        continue;
+      }
+      this[counter] = arr[j];
+      counter++;
+    }
+
+    numberOfItems += arr.length - 1;
+  }
+  this.length = numberOfItems;
+
+  const objLength = Object.keys(this).length;
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+
   return this;
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
 import JSL from './lib/lib';
 
 
+// JSL('button').on('click', function () {
+//   JSL('div').eq(1).toggleClass('active');
+// });
 
+// JSL('div').click(function () {
+//   console.log(JSL(this).index());
+// });
 
+// // console.log(JSL('div').eq(2).find('.some'));
+console.log(JSL('.some').closest('.findme').addClass('dad'));
 
-
-
+// console.log(JSL('div'));
+// console.log(JSL('.findme').siblings());
