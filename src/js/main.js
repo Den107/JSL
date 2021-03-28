a3add8b48585b092ead1f9d8d6dc75a87e12bfe1
Den107 <deniskrv@gmail.com>
2021-03-28 18:29:32 +0300

    add fadeToggle function

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ import JSL from './lib/lib';
 
 
 JSL('#first').on('click', () => {
-  JSL('div').eq(1).fadeOut(800);
+  JSL('div').eq(1).fadeToggle(800);
 });
 
 JSL('[data-count="second"]').on('click', () => {
-  JSL('div').eq(2).fadeOut(800);
+  JSL('div').eq(2).fadeToggle(800);
 });
 
 JSL('button').eq(2).on('click', () => {
-  JSL('.w-500').fadeOut(800);
+  JSL('.w-500').fadeToggle(800);
 });
