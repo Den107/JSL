84f5d8fc203548d2f7dbeb7087d522d51f639052
Den107 <deniskrv@gmail.com>
2021-03-29 17:40:13 +0300

    add slider function

---
+++ src/js/lib/components/carousel.js
@@ -0,0 +1,71 @@
+import JSL from '../core';
+
+JSL.prototype.carousel = function () {
+  for (let i = 0; i < this.length; i++) {
+    const width = window.getComputedStyle(this[i].querySelector('.carousel-inner')).width;
+    const slides = this[i].querySelectorAll('.carousel-item');
+    const slidesField = this[i].querySelector('.carousel-slides');
+    const dots = this[i].querySelectorAll('.carousel-indicators li');
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slides.forEach(slide => {
+      slide.style.width = width;
+    });
+
+    let offset = 0;
+    let slideIndex = 0;
+
+    JSL(this[i].querySelector('[data-slide="next"]')).click((e) => {
+      e.preventDefault();
+      if (offset == (+width.replace(/\D/g, '') * (slides.length - 1))) {
+        offset = 0;
+      } else {
+        offset += +width.replace(/\D/g, '');
+      }
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slideIndex == slides.length - 1) {
+        slideIndex = 0;
+      } else {
+        slideIndex++;
+      }
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[slideIndex].classList.add('active');
+    });
+
+    JSL(this[i].querySelector('[data-slide="prev"]')).click((e) => {
+      e.preventDefault();
+      if (offset == 0) {
+        offset = +width.replace(/\D/g, '') * (slides.length - 1);
+      } else {
+        offset -= +width.replace(/\D/g, '');
+      }
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slideIndex == 0) {
+        slideIndex = slides.length - 1;
+      } else {
+        slideIndex--;
+      }
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[slideIndex].classList.add('active');
+    });
+
+    const sliderId = this[i].getAttribute('id');
+    JSL(`#${sliderId} .carousel-indicators li`).click(e => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+      offset = +width.replace(/\D/g, '') * slideTo;
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[slideIndex].classList.add('active');
+    });
+  }
+};
+
+JSL('.carousel').carousel();
